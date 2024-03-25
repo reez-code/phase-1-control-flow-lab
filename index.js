e8c7c83050cf7692776dce1d7e557b3399f206b7
Phil <phil.github@gmail.com>
2024-03-25 19:18:01 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+     // Write your code here!
+     if (ride <= 400){
+     return 'This one is on me!';
+     } else if (ride > 400 && ride <= 2000){
+     return 'That will be twenty bucks.';
+     } else if (ride > 2000 && ride <= 2500){
+       return 'I will gladly take your thirty bucks.'
+     } else if (ride > 2500){
+      return 'No can do.'
+    }
+  }
+  function ternaryCheckCity(city){
+   // Write your code here!
+    let checkCity = city === "NYC" ? "Ok, sounds good." : 'No go.';
+      return checkCity;
+ 
+  }
+  function switchOnCharmFromTip(tip){
+   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return "Bye.";
+      break;
+   }
+  }
